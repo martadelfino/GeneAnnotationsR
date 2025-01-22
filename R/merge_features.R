41118366d61e99130802511fa297d0c20431d989
Marta <martadelfino23@gmail.com>
2025-01-22 15:17:53 +0000

    changed ` back to " in merging function

--- R/merge_features.R
+++ R/merge_features.R
@@ -9,7 +9,7 @@
 merge_features <- function(df_list) {
 
   # Use Reduce to iteratively merge each data frame in the list
-  merged_df <- Reduce(function(x, y) merge(x, y, by = `hgnc_id`, all = TRUE), df_list)
+  merged_df <- Reduce(function(x, y) merge(x, y, by = "hgnc_id", all = TRUE), df_list)
 
   return(merged_df)
 }
