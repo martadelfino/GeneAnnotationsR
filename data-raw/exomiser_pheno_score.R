8007b5bf6bcc6685e9c928b400961adb1320ce66
Marta <martadelfino23@gmail.com>
2025-01-22 10:35:07 +0000

    made exomiser_pheno_score rdata internal only

--- data-raw/exomiser_pheno_score.R
+++ data-raw/exomiser_pheno_score.R
@@ -2,4 +2,4 @@
 
 load('./data/NDD_Exomiser_Pheno_Score.RData')
 
-usethis::use_data(exomiser_pheno_score, overwrite = TRUE)
+usethis::use_data(exomiser_pheno_score, internal = TRUE, overwrite = TRUE)
